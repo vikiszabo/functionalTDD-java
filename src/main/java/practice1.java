f613871274566b37db6b26bb26a0d25d018914db
weiliya <viktoria.szabo.cv@gmail.com>
2019-02-03 21:55:32 +0100

    string test

--- src/main/java/practice1.java
+++ src/main/java/practice1.java
@@ -5,4 +5,8 @@ class practice1 {
     public static Integer getFirstEven(Stream<Integer> stream) {
         return stream.filter(integer -> integer % 2 == 0).findFirst().get();
     }
+
+    public static String getFirstEvenString(Stream<Integer> stream) {
+        return stream.filter(integer -> integer % 2 == 0).findFirst().get().toString();
+    }
 }
--- src/test/java/practice1test.java
+++ src/test/java/practice1test.java
@@ -11,4 +11,11 @@ class practice1test {
         Integer firstEven = practice1.getFirstEven(Arrays.asList(1, 3, 4, 7, 6).stream());
         assertEquals(new Integer(4), firstEven);
     }
+
+    @Test
+    public void TestGetFirstEvenNumberString() {
+        String firstEven = practice1.getFirstEvenString(Arrays.asList(1, 3, 4, 7).stream());
+        assertEquals("4", firstEven);
+    }
+
 }
